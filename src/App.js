32c3218bd8638abaaa3b524bf9418ba1d683da1f
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-06-24 19:17:16 -0700

    Setup firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD8LhyQFVI1E0QKf5aKizjyS-311euedIw',
+      authDomain: 'auth-91fef.firebaseapp.com',
+      databaseURL: 'https://auth-91fef.firebaseio.com',
+      projectId: 'auth-91fef',
+      storageBucket: 'auth-91fef.appspot.com',
+      messagingSenderId: '1019160475011'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>here</Text>
+        <Header headerText="Authentication" />
+        <Text>An App!!!</Text>
       </View>
     );
   }
